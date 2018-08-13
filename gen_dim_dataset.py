f8237d3730facec4dde585c8c0f647b1fe95b2dc
tim885 <xcqiutim@outlook.com>
2018-08-13 19:08:32 +0200

    update

---
+++ gen_dim_dataset.py
@@ -0,0 +1,117 @@
+# generate dataset configuration file for block dimension(L,W,H) estimation
+# from basic dataset
+# created by QIU Xuchong
+# 2018/08
+
+import argparse  # module for user-friendly command-line interfaces
+import pandas as pd  # easy csv parsing
+import numpy as np
+import matplotlib.pyplot as plt  # for visualization
+import time
+import warnings
+
+
+# command-line interface arguments
+parser = argparse.ArgumentParser(description='Script for generating block dimension estimation dataset')
+parser.add_argument('--csv_path', default='/home/xuchong/ssd/Projects/block_estimation/DATA/UnrealData/'
+                                          'scenario_PV3.1/CSV_files/20171214-104147_data.txt',
+                    type=str, help='directory containing dataset csv files')
+parser.add_argument('--numClasses', default=[10, 10, 10], type=int,
+                    help='number of class for each classification task')
+parser.add_argument('--bin_size', default=7.5, type=int,
+                    help='bin size of classification in regression task ')
+parser.add_argument('--train_ratio', default=0.8, type=int,
+                    help='ratio of training set on whole dataset')
+
+
+def main():
+    global args
+    args = parser.parse_args()
+    samples = pd.read_csv(args.csv_path)
+
+    # fetch sample path and dimension labels
+    imgs_path = samples['{path}']  # or samples.path
+    block_length = samples['{BlockSizeX}']
+    block_width = samples['{BlockSizeY}']
+    block_height = samples['{BlockSizeZ}']
+
+    # histogram of dimension classes in original dataset
+    block_length.hist(bins=50)
+    plt.xlabel('block size x(cm)')
+    plt.show()
+    block_width.hist(bins=50)
+    plt.xlabel('block size y(cm)')
+    plt.show()
+    block_height.hist(bins=50)
+    plt.xlabel('block size Z(cm)')
+    plt.show()
+
+    # assign new labels for new dataset configuration file
+    size_x_label = block_length*10
+    size_y_label = block_width*10
+    size_z_label = block_height*10
+
+    dim_min = 25
+    dim_max = 100
+
+    for class_idx in range(0, args.numClasses[0]):
+        interval_min = dim_min + args.bin_size*class_idx
+        interval_max = dim_min + args.bin_size*(class_idx+1)
+
+        size_x_label[(size_x_label >= interval_min) & (size_x_label < interval_max)] = class_idx
+        size_y_label[(size_y_label >= interval_min) & (size_y_label < interval_max)] = class_idx
+        size_z_label[(size_z_label >= interval_min) & (size_z_label < interval_max)] = class_idx
+
+    size_x_label[size_x_label == dim_max] = (args.numClasses[0] - 1)
+    size_y_label[size_y_label == dim_max] = (args.numClasses[0] - 1)
+    size_z_label[size_z_label == dim_max] = (args.numClasses[0] - 1)
+
+    # histogram of dimension classes in dimension estimation dataset
+    size_x_label.hist()
+    plt.xlabel('block size x on dataset')
+    plt.show()
+    size_y_label.hist()
+    plt.xlabel('block size y on dataset')
+    plt.show()
+    size_z_label.hist()
+    plt.xlabel('block size z on dataset')
+    plt.show()
+
+    # save in dataset csv file
+    df_dim = pd.DataFrame({'path': imgs_path,
+                           'block_dim_x': size_x_label,
+                           'block_dim_y': size_y_label,
+                           'block_dim_z': size_z_label})
+
+    msk = np.random.rand(len(df_dim)) < args.train_ratio
+    df_dim_train = df_dim[msk]
+    df_dim_val = df_dim[~msk]
+
+    # histogram for train/val set
+    df_dim_train['block_dim_x'].hist(bins=args.numClasses[0])
+    plt.xlabel('block size x for train')
+    plt.show()
+    df_dim_train['block_dim_y'].hist(bins=args.numClasses[0])
+    plt.xlabel('block size y for train')
+    plt.show()
+    df_dim_train['block_dim_z'].hist(bins=args.numClasses[0])
+    plt.xlabel('block size for train')
+    plt.show()
+
+    df_dim_val['block_dim_x'].hist(bins=args.numClasses[0])
+    plt.xlabel('block size x for val')
+    plt.show()
+    df_dim_val['block_dim_y'].hist(bins=args.numClasses[0])
+    plt.xlabel('block size y for val')
+    plt.show()
+    df_dim_val['block_dim_z'].hist(bins=args.numClasses[0])
+    plt.xlabel('block size z for val')
+    plt.show()
+
+    date_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())  # current date
+    df_dim_train.to_csv(date_time+'_train.txt', index=False)
+    df_dim_val.to_csv(date_time + '_val.txt', index=False)
+
+
+if __name__ == '__main__':
+    main()
