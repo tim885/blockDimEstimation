ed90d95ad1e412b622615c13b8d6110354a6f1c6
tim885 <xcqiutim@outlook.com>
2018-09-18 15:38:38 +0200

    upload result

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+#CUDA_VISIBLE_DEVICES=2 python dim_estimation.py --dataset_name '2018-08-14_15-25-55' --epochs 50 --batch-size 128 --lr 0.01 --gpu 2
+
+#CUDA_VISIBLE_DEVICES=2 python dim_estimation.py --dataset_name '2018-08-14_15-25-55' --resume 'results/checkpoint.pth.tar' --epochs 50 --batch-size 128 --lr 0.005 --gpu 2
+
 CUDA_VISIBLE_DEVICES=2 python dim_estimation.py --dataset_name '2018-08-14_15-25-55' --epochs 50 --batch-size 128 --lr 0.01 --gpu 2
-
-CUDA_VISIBLE_DEVICES=2 python dim_estimation.py --dataset_name '2018-08-14_15-25-55' --resume 'results/checkpoint.pth.tar' --epochs 50 --batch-size 128 --lr 0.005 --gpu 2
-
-
 
 
